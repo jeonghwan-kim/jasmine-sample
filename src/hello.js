481e1bf07778c346a6185cf433528c9d69e493cb
김정환 <6pack@woowahan.com>
2017-03-26 19:28:16 +0900

    ajax test

--- src/hello.js
+++ src/hello.js
@@ -3,7 +3,20 @@ var Hello = {
   greeting() {
     return this.message + ' ' + this.getName();
   },
-  getName() {
+  getName(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', 'http://name', true);
+    req.onreadystatechange = function () {
+      if (req.readyState == 4) {
+        if(req.status == 200) {
+          cb(req.responseText);
+        } else {
+          cb("Error loading page\n");
+        }
+      }
+    };
+    req.send(null);
+
     return this.name || 'world';
   }
 };
--- test/hello.spec.js
+++ test/hello.spec.js
@@ -11,4 +11,33 @@ describe('Hello모듈의', ()=> {
       expect(Hello.getName).toHaveBeenCalled();
     });
   });
+  describe('getName함수는', ()=>{
+    let request,
+        callbackSpy,
+        response;
+    beforeEach(()=> {
+      jasmine.Ajax.install();
+      callbackSpy = jasmine.createSpy('callback');
+      Hello.getName(callbackSpy);
+      request = jasmine.Ajax.requests.mostRecent();
+      response = {
+        status: 200,
+        responseText: 'Chris'
+      };
+      request.respondWith(response);
+    });
+    afterEach(()=> {
+      jasmine.Ajax.uninstall();
+    });
+    it('HTTP 요청을 보낸다', () => {
+      const expectUrl = 'http://name';
+      expect(request.url).toBe(expectUrl);
+    });
+    it('http 응답이 오면 콜백함수를 실행한다', ()=> {
+      expect(callbackSpy).toHaveBeenCalled();
+    });
+    it('콜백함수 파라매터로 이름을 반환한다', ()=> {
+      expect(callbackSpy).toHaveBeenCalledWith(response.responseText);
+    });
+  });
 });
